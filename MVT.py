1fb60141575e3bdd262ee84c54093b285ae1b0e0
HaotianXIAO <82371455+HaotianXIAO@users.noreply.github.com>
2021-10-17 21:14:24 +0200

    Update MVT.py

--- MVT.py
+++ MVT.py
@@ -1,28 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-from __future__ import division 
 import numpy as np
 import matplotlib.pyplot as plt 
-import matplotlib.axes as As 
 from math import *
 
 # Constants
 l=900           #mm
-b=75            #mm
+b=87            #mm
 h=150           #mm
 e=3             #mm
 O=(b-e)*(h-e)   #mm2
-I=(b*h**3-(b-2*e)*(h-2*e)**3)/12    #mm4
-P=100           #N
-ftu=fcu=10 #MPa
-E=10000 #Mpa
-
-
-# In[ ]:
+I=(e * (h**3))/6 + 0.5*e*(b-e)*(h-e)*(h-e) #mm4
+P=400           #N
 
 
 def M(x3):
@@ -35,6 +22,13 @@ def M(x3):
     else :
         return 0
 
+M_x=[]
+M_y=[]
+for j in range(0,l,1):
+    M_x.append(j)
+    M_y.append(M(j)) 
+plt.figure(1)
+diagrame_m = plt.plot(M_x, M_y)
 
 def T(x3):
     if 0 <=x3<(l/3):
@@ -45,6 +39,14 @@ def T(x3):
         return ((1/0.364)*P*l)/6 
     else:
         return 0
+
+T_x=[]
+T_y=[]
+for j in range(0,l,1):
+    T_x.append(j)
+    T_y.append(T(j)) 
+plt.figure(2)
+diagrame_v = plt.plot(T_x, T_y)
     
 def V(x3):
     if 0 <=x3<(l/3):
@@ -56,6 +58,10 @@ def V(x3):
     else:
         return 0
 
-
-
-
+V_x=[]
+V_y=[]
+for j in range(0,l,1):
+    V_x.append(j)
+    V_y.append(V(j)) 
+plt.figure(3)
+diagrame_V = plt.plot(V_x, V_y)
